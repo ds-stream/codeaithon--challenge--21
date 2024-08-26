3ca5141dca644a6b8ba18f7194890180f5253994
DSS-KKacprzak <kk@LAPWAW030505.>
2024-08-26 09:43:35 +0200

    Final

--- tests/automatic_task_verification.py
+++ tests/automatic_task_verification.py
@@ -1,10 +1,13 @@
 import os
-import importlib.util
 import pytest
+import random
+import time
+from pathlib import Path
+from tqdm import tqdm
 
 
 def test_submission_files_exist(developer):
-    base_path = f"submissions/developer_{developer}"
+    base_path = f"submissions/developer-{developer}"
     folders = ["tests", "results"]
 
     for folder in folders:
@@ -14,3 +17,73 @@ def test_submission_files_exist(developer):
 
         files = os.listdir(os.path.join(base_path, folder))
         assert len(files) > 0, f"No {folder} files found"
+
+
+# correctness
+TEST_TIMEOUT = 10 * 60  # 10 minutes
+
+
+# --------------- reference implementation --------------------- #
+def reference_is_prime(n: int) -> bool:
+    """Check if a number is prime - this is a reference implementation."""
+    # source: https://stackoverflow.com/a/46844461/3081328
+    if n == 1 or n & 1 == 0:
+        return False
+    d = 3
+    while d * d <= n:
+        if n % d == 0:
+            return False
+        d = d + 2
+    return True
+
+
+# -------------------------------------------------------------- #
+
+
+def test_edge_conditions(solution):
+    """Test edge conditions."""
+    assert not solution.is_prime(1), "1 is not a prime number"
+    assert solution.is_prime(2), "2 is a prime number"
+    assert solution.is_prime(3), "3 is a prime number"
+    assert not solution.is_prime(4), "4 is not a prime number"
+    assert solution.is_prime(5), "5 is a prime number"
+    assert solution.is_prime(13), "13 is a prime number"
+    assert not solution.is_prime(99_987), "99,987 is not a prime number"
+    assert solution.is_prime(99_989), "99,989 is a prime number"
+    assert solution.is_prime(99_991), "99,991 is a prime number"
+    assert not solution.is_prime(100_000), "100,000 is not a prime number"
+
+
+def test_results_in_finite_time(solution):
+    """Test that the solution runs in finite time."""
+    start = time.time()
+
+    with tqdm(total=250) as bar:
+        for _ in range(250):
+            n = random.randint(1, 100_000)
+            assert solution.is_prime(n) is reference_is_prime(
+                n
+            ), f"Result for {n} is incorrect"
+
+            if time.time() - start > TEST_TIMEOUT:
+                raise RuntimeError(
+                    "Execution time exceeded. Your implementation is too slow!"
+                )
+
+            bar.update(1)
+
+
+def test_not_using_forbidden_instructions(code):
+    assert "for" not in code, "Used forbidden instruction: `for`"
+    assert "while" not in code, "Used forbidden instruction: `while`"
+    assert "filter" not in code, "Used forbidden instruction: `filter`"
+
+
+def test_check_initial_conditions(solution):
+    with pytest.raises(ValueError):
+        solution.is_prime(0)
+
+    with pytest.raises(ValueError):
+        solution.is_prime(100_001)
+
+    assert not solution.is_prime(1)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+import os
+import importlib.util
+from pathlib import Path
 
 
 def pytest_addoption(parser):
@@ -10,4 +13,36 @@ def developer(request):
     return request.config.getoption("--developer")
 
 
-# run pytest --developer=<number>
+def import_solution_module(developer):
+    solution_path = os.path.join(
+        "submissions", f"developer-{developer}", "results", "solution.py"
+    )
+    assert os.path.exists(
+        solution_path
+    ), f"Solution file {solution_path} does not exist."
+
+    spec = importlib.util.spec_from_file_location("solution", solution_path)
+    solution = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(solution)
+    return solution
+
+
+@pytest.fixture(scope="session")
+def solution(request):
+    developer = request.config.getoption("--developer")
+    solution = import_solution_module(developer)
+    return solution
+
+
+@pytest.fixture(scope="session")
+def code(developer) -> str:
+    solution_path = os.path.join(
+        "submissions", f"developer-{developer}", "results", "solution.py"
+    )
+    data = solution_path.read_text().split("\n")
+    return "".join(
+        line for line in data if not line.strip() or not line.strip().startswith("#")
+    )
+
+
+# run pytest tests/autoamtic_task_verification.py --developer=<number>
